a22915497a7882d124ad73e253ccdf0fbf8edb20
Z1Park <dongchen119@naver.com>
2025-01-17 04:07:47 +0900

    [FEAT] 결제 동시성 테스트 작성(실패) : 원인을 알 수 없는 에러 발생하는 상황... 추후 revert를 위해 커밋 분리

--- src/test/java/kr/hhplus/be/server/application/reservation/ReservationFacadeServiceConcurrencyTest.kt
+++ src/test/java/kr/hhplus/be/server/application/reservation/ReservationFacadeServiceConcurrencyTest.kt
@@ -3,9 +3,13 @@ package kr.hhplus.be.server.application.reservation
 import kr.hhplus.be.server.ConcurrencyTestHelper
 import kr.hhplus.be.server.TestContainerCleaner
 import kr.hhplus.be.server.common.component.ClockHolder
+import kr.hhplus.be.server.common.exception.CustomException
 import kr.hhplus.be.server.domain.concert.Concert
 import kr.hhplus.be.server.domain.concert.ConcertSchedule
 import kr.hhplus.be.server.domain.concert.ConcertSeat
+import kr.hhplus.be.server.domain.queue.Queue
+import kr.hhplus.be.server.domain.queue.QueueActiveStatus
+import kr.hhplus.be.server.domain.reservation.Reservation
 import kr.hhplus.be.server.domain.user.User
 import kr.hhplus.be.server.infrastructure.concert.ConcertJpaRepository
 import kr.hhplus.be.server.infrastructure.concert.ConcertScheduleJpaRepository
@@ -24,6 +28,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import java.time.LocalDateTime
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
 @Import(TestConfig::class)
@@ -78,6 +84,55 @@ class ReservationFacadeServiceConcurrencyTest(
 		//then
 		val actualReservations = reservationJpaRepository.findAll()
 		assertThat(actualReservations).hasSize(1)
+	}
+
+	/**
+	 * ThreadLocal로 인해 코루틴 사용 불가
+	 */
+	@Test
+	fun `결제 요청 시, 동일한 예약에 대해 3번의 요청이 동시에 들어와도 결제는 1개만 생성되어야 한다`() {
+		// given
+		val testTime = LocalDateTime.of(2025, 1, 17, 12, 59, 59)
+
+		val userUUID = "myUserUUID"
+		val user = User("김항해", userUUID, 60000)
+		userJpaRepository.save(user)
+
+		val seat = ConcertSeat(99, 15000, 2L, testTime.plusMinutes(5))
+		concertSeatJpaRepository.save(seat)
+
+		val reservation = Reservation(testTime, 15000, user.id, 1L, 2L, seat.id)
+		reservationJpaRepository.save(reservation)
+
+		val tokenUUID = "myTokenUUID"
+		val token = Queue(userUUID, tokenUUID, QueueActiveStatus.ACTIVATED, testTime.plusDays(1))
+		queueJpaRepository.save(token)
+
+		val cri1 = PaymentCri.Create(userUUID, tokenUUID, reservation.id)
+		val cri2 = PaymentCri.Create(userUUID, tokenUUID, reservation.id)
+		val cri3 = PaymentCri.Create(userUUID, tokenUUID, reservation.id)
+		val cris = listOf(cri1, cri2, cri3)
+
+		val countDownLatch = CountDownLatch(3)
+		val executors = Executors.newFixedThreadPool(3)
+
+		// when
+		for (i in 0 until 1) {
+			executors.execute {
+				try {
+					sut.payReservation(cris[i])
+				} catch (e: Exception) {
+					if (e is CustomException)
+						println(e.errorCode.message)
+					else println(e)
+				}
+				countDownLatch.countDown()
+			}
+		}
+
+		//then
+		val actualPayments = paymentJpaRepository.findAll()
+		assertThat(actualPayments).hasSize(1)
 	}
 }
 
