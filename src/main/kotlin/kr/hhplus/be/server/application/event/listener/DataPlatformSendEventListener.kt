eafabebbd0e6e8c8107fc30f655cb5bd4a9f042b
Z1Park <dongchen119@naver.com>
2025-02-17 19:03:34 +0900

    [FIX] 트랜잭션 없는 로직에서 발행하는 이벤트 리스너 TransactionalEventListener를 EventListener로 변경

--- src/main/kotlin/kr/hhplus/be/server/application/event/listener/DataPlatformSendEventListener.kt
+++ src/main/kotlin/kr/hhplus/be/server/application/event/listener/DataPlatformSendEventListener.kt
@@ -2,10 +2,9 @@ package kr.hhplus.be.server.application.event.listener
 
 import kr.hhplus.be.server.application.event.DataPlatformSendEvent
 import kr.hhplus.be.server.domain.external.DataPlatformApiClient
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.transaction.event.TransactionPhase
-import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class DataPlatformSendEventListener(
@@ -16,7 +15,7 @@ class DataPlatformSendEventListener(
 	 * 결제 완료 이후(AFTER_COMMIT), 데이터 플랫폼으로 결제 내역 전송
 	 */
 	@Async
-	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@EventListener
 	fun sendDataToDataPlatform(dataPlatformSendEvent: DataPlatformSendEvent) {
 		dataPlatformApiClient.send(
 			dataPlatformSendEvent.concertSeatId,
